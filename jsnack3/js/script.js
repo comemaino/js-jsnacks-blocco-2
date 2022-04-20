ea1e69af16c84df9f522c20835cbc87807df8b3c
comemaino <mat.maino@gmail.com>
2022-04-20 13:41:54 +0200

    Add: Jsnack3 script

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,1 +1,12 @@
 //Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
+
+//CHIEDI UN NUMERO ALL'UTENTE
+const n = prompt("dimmi un numero");
+
+let numbers = [];
+
+for (let i = 0; i < n; i++) {
+  const cubes = Math.pow([numbers], 3);
+  console.log(cubes);
+  numbers++;
+}
