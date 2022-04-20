94a9d15e8cca1e002c135326f1da6118b13d8ca5
comemaino <mat.maino@gmail.com>
2022-04-20 13:31:10 +0200

    Add: Jsnack2 script

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,3 +1,16 @@
 //Generatore di "nomi cognomi" casuali: prendendo una lista di nomi e una
 // lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 
+const firstNames = ["Paolo", "Luca", "Maria", "Sofia", "Lara", "Massimo"];
+const lastNames = ["Bianchi", "Rossi", "Barcella", "Bellenghi", "Forni", "Crovetto"]
+
+for (let i = 0; i < 3; i++) {
+  const randomFirstNameIndex = (Math.floor(Math.random()*firstNames.length));
+  const randomFirstName = firstNames[randomFirstNameIndex];
+
+  const randomLastNameIndex = (Math.floor(Math.random()*lastNames.length));
+  const randomLastName = lastNames[randomLastNameIndex];
+
+  const randomGuest = randomFirstName + " " + randomLastName;
+  console.log(randomGuest);
+}
